3834c4dea2c584e3db2d29a903da2d75b1a9b199
rumesh <rumesh.m.s16@gmail.com>
2019-11-07 23:56:12 +0530

    routes fixed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
     app.use("/user", require("./user"));
-    app.use("/item", require("./question"));
     app.use("/question", require("./question"));
+    app.use("/answer",require("./answer"));
     app.use("/", (req, res) => {
         res.json({"message": "Welcome to the API"})
     });
