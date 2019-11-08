8eb9f375267077c93fdc516bcbc775b3e239bc46
rumesh <rumesh.m.s16@gmail.com>
2019-11-08 06:00:52 +0530

    cors

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ module.exports = function (app) {
     app.use("/question", require("./question"));
     app.use("/answer",require("./ans"));
     app.use("/tag",require('./tag'));
+    app.use("/votes",require('./votes'));
     app.use("/", (req, res) => {
         res.json({"message": "Welcome to the API"})
     });
--- socket_server.js
+++ socket_server.js
@@ -1,10 +1,4 @@
-const server = require("net").createServer();
-const io = require("socket.io")(server);
 
-const handleClient = function (socket) {
-    // we've got a client connection
-    socket.emit("tweet", {user: "nodesource", text: "Hello, world!"});
-};
+const io = require("socket.io")(3002);
 
-io.on("connection", handleClient);
-server.listen(3002);
+
